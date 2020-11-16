1ec6cc6e94c85cfa555fafd542f2948515783477
SOGAKEN <58077058+SOGAKEN@users.noreply.github.com>
2020-11-17 02:15:52 +0900

    header

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import styled from 'styled-components';
+import Header from '../assets/header';
 
 export default function Home() {
 	return (
@@ -9,6 +10,8 @@ export default function Home() {
 				<title>Create Next App</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
+
+			<Header />
 
 			<main className={styles.main}>
 				<h1 className={styles.title}>
