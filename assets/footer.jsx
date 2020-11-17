bcf9f5ac224da4ed90c37fcb098570ddbcc235df
SOGAKEN <58077058+SOGAKEN@users.noreply.github.com>
2020-11-17 11:27:52 +0900

    style

--- assets/footer.jsx
+++ assets/footer.jsx
@@ -1,38 +1,23 @@
 import React from 'react';
-import Link from 'next/link';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
 import styled from 'styled-components';
 
-const Header =()=>{
-	return(
-		<header>
-			<nav>
-				<input type="checkbox" id="checkbox"/>
-				<label for="checkbox" className="checkbtn">
-					<FontAwesomeIcon icon={faBars} />
-				</label>
-				<Tittle><Link href='/'><a>issonuts</a></Link></Tittle>
-				<ul>
-					<li><a href="#">About</a></li>
-					<li><Link href='/contactme' as='contactme.html'><a>Contact Me</a></Link></li>
-				</ul>
+const Footer =()=>{
 
-			</nav>
-		</header>
+	let now = new Date();
+	let year = now.getFullYear();
+	
+	return(
+		<Foot>&copy;{year} issonuts</Foot>
 	)
+
+	
 }
 
-export default Header;
+export default Footer;
 
-const Tittle = styled.label`
-	color: #0DF2C9;
-	font-size: 35px;
-	line-height: $height;
-	padding: 0 100px;
-	font-weight: bold;
-	font-family: 'Abril Fatface', cursive;
-	a{
-		color: inherit;
-	}
+const Foot = styled.footer`
+	height: 50px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 `;
